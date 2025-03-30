1a1679a8e5c5dbfd17da2b227fe4be7bc1bf6553
emilio <ecardilloschrader@gmail.com>
2025-03-30 01:57:10 -0400

    kanban working

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import ProfileViewSet, MedicalRecordViewSet
+from .views import ProfileViewSet, MedicalRecordViewSet, TaskViewSet
 
 router = DefaultRouter()
 router.register(r'profiles', ProfileViewSet, basename='profile')
 router.register(r'profiles/(?P<profile_id>\d+)/records', MedicalRecordViewSet, basename='profile-records')
+router.register(r'profiles/(?P<profile_id>\d+)/tasks', TaskViewSet, basename='profile-tasks')
+router.register(r'tasks', TaskViewSet, basename='task')
 
 urlpatterns = [
     path('', include(router.urls)),
